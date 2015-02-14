62e8ac302fa844151e6c3bcec9fb539e361e6210
velcrome <velcrome@gmx.net>
2015-02-14 20:02:50 +0100

    dynamic LINQ plugin

---
+++ patches/plugins/SearchMessage.SpreadTemplate/MessageSearchTemplateNode.cs
@@ -0,0 +1,67 @@
+using System.ComponentModel.Composition;
+
+using System.Linq;
+using System.Linq.Dynamic;
+
+using VVVV.Core.Logging;
+using VVVV.Packs.Messaging; 
+using VVVV.PluginInterfaces.V2;  
+using VVVV.Utils;
+
+
+namespace VVVV.Packs.Messaging.Nodes
+{
+    [PluginInfo(Name = "SearchTemplate", Category = "Message.Spread", Help = "Allows LINQ queries for Messages", Tags = "LINQ", Author = "velcrome")]
+    public class MessageSearchNode : IPluginEvaluate
+    {
+#pragma warning disable 649, 169
+        [Input("Input")]
+        private IDiffSpread<Message> FInput; 
+
+        [Output("Message", AutoFlush = false)]
+        private ISpread<Message> FOutput;
+
+        [Output("Former Slice", AutoFlush = false)]
+        private ISpread<int> FFormerSlice;
+
+        [Import()]
+        protected ILogger FLogger;
+#pragma warning restore
+
+        public void Evaluate(int SpreadMax)
+        {
+			if (Prep(out SpreadMax)) return;
+
+        	var result = 
+        					from message in FInput
+        					let foo = (string)message["Foo"][0]
+        					where foo != "bar"
+        					select message;
+
+             FOutput.SliceCount = 0;
+            FOutput.AssignFrom(result.ToArray());
+            FOutput.Flush();
+        }
+    	
+		protected bool Prep(out int SpreadMax) {
+            if (FInput.SliceCount <= 0 || FInput[0] == null)
+                SpreadMax = 0;
+            else SpreadMax = FInput.SliceCount;
+
+            if (SpreadMax == 0)
+            {
+                if (FOutput.SliceCount != 0)
+                {
+                    FOutput.SliceCount = 0;
+                    FOutput.Flush();
+                }
+                return true;
+            }
+			if (!FInput.IsChanged) return true;
+			
+			return false;
+		}			
+
+
+    }
+}
---
+++ src/MessageNodes/Spread/MessageSortNode.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.Composition;
+using System.Linq;
+using VVVV.Core.Logging;
+using VVVV.Packs.Messaging;
+using VVVV.PluginInterfaces.V2;
+using VVVV.Utils;
+
+namespace VVVV.Pack.Messaging.Nodes
+{
+    #region PluginInfo
+    [PluginInfo(Name = "Sort", Category = "Message", Help = "Filter Messages", Tags = "Time", Author = "velcrome")]
+    #endregion PluginInfo
+    public class MessageSortNode : IPluginEvaluate
+    {
+#pragma warning disable 649, 169
+        [Input("Input")]
+        private IDiffSpread<Message> FInput;
+
+        [Output("Output", AutoFlush = false)]
+        private ISpread<Message> FOutput;
+
+        [Import()]
+        protected ILogger FLogger;
+
+#pragma warning restore
+
+        public void Evaluate(int SpreadMax)
+        {
+            if (FInput.IsAnyInvalid())
+                SpreadMax = 0;
+            else SpreadMax = FInput.SliceCount;
+
+            if (SpreadMax == 0)
+            {
+                if (FOutput.SliceCount != 0)
+                {
+                    FOutput.SliceCount = 0;
+                    FOutput.Flush();
+                }
+                return;
+            }
+
+            if (!FInput.IsChanged) return;
+
+
+            FOutput.SliceCount = 0;
+            FOutput.AssignFrom( FInput.OrderBy(message => message.TimeStamp));
+
+            FOutput.Flush();
+        }
+    }
+}
