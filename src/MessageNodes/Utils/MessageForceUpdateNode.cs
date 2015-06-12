bc84eaff53e8f17932c50bf834f77ffbceefc668
velcrome <marko@intolight.de>
2015-06-12 17:43:18 +0200

    FlowControl node

--- src/MessageNodes/Utils/MessageForceUpdateNode.cs
+++ src/MessageNodes/Utils/MessageForceUpdateNode.cs
@@ -8,17 +8,19 @@ using VVVV.Utils;
 
 namespace VVVV.Packs.Messaging.Nodes
 {
+    public enum FlowControlEnum { Default, Force, Block};
+
     #region PluginInfo
-    [PluginInfo(Name = "ForceUpdate", Category = "Message", Help = "Enforces downstream Update", AutoEvaluate = true, Author = "velcrome")]
+    [PluginInfo(Name = "FlowControl", Category = "Message", Help = "Enforces downstream Update", AutoEvaluate = true, Author = "velcrome")]
     #endregion PluginInfo
-    public class MessageForceUpdateNode : IPluginEvaluate
+    public class MessageFlowControlNode : IPluginEvaluate
     {
 #pragma warning disable 649, 169
         [Input("Input")]
         private IDiffSpread<Message> FInput;
 
-        [Input("Update", IsSingle=true)]
-        private IDiffSpread<bool> FUpdate;
+        [Input("Update", IsSingle=true, DefaultEnumEntry = "Block")]
+        private IDiffSpread<FlowControlEnum> FFlowControl;
 
         [Output("Output", AutoFlush = false)]
         private ISpread<Message> FOutput;
@@ -29,7 +31,10 @@ namespace VVVV.Packs.Messaging.Nodes
 
         public void Evaluate(int SpreadMax)
         {
-            if (FInput.IsChanged || FUpdate[0])
+            var flow = FFlowControl[0];
+            if ((FInput.IsChanged && flow == FlowControlEnum.Default) || // flow only when input changed
+                flow == FlowControlEnum.Force || // always flow
+                (flow == FlowControlEnum.Default && FFlowControl.IsChanged)) // also flow, when FlowControl resumed
             {
                 FOutput.SliceCount = 0;
                 FOutput.AssignFrom(FInput);
