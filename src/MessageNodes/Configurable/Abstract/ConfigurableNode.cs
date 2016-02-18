b87309fbbc5752d3726c568620343e4b5b022761
velcrome <marko@gmx.net>
2016-02-18 23:48:22 +0100

    first working flowgui

--- src/MessageNodes/Configurable/Abstract/ConfigurableNode.cs
+++ src/MessageNodes/Configurable/Abstract/ConfigurableNode.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.Composition;
+using System.Drawing;
+using System.Windows.Forms;
 using VVVV.Core.Logging;
 using VVVV.PluginInterfaces.V2;
 
 namespace VVVV.Packs.Messaging.Nodes
 {
-    public abstract class ConfigurableNode :  IPluginEvaluate, IPartImportsSatisfiedNotification
+    public abstract class ConfigurableNode :  UserControl, IPluginEvaluate, IPartImportsSatisfiedNotification
     {
         [Config("Configuration", DefaultString = "string Foo")]
         public IDiffSpread<string> FConfig;
@@ -12,15 +14,110 @@ namespace VVVV.Packs.Messaging.Nodes
         [Import()]
         protected ILogger FLogger;
 
+        FlowLayoutPanel FWindow = new FlowLayoutPanel();
+
+        protected abstract void HandleConfigChange(IDiffSpread<string> configSpread);
+        public abstract void Evaluate(int SpreadMax);
+
+        public ConfigurableNode()
+        {
+            InitializeWindow();
+            LayoutByCount();
+        }
+
 
         public virtual void OnImportsSatisfied()
         {
             FConfig.Changed += HandleConfigChange;
         }
 
+        public void InitializeWindow() {
+            //table panel fills the whole window
+            FWindow.FlowDirection = FlowDirection.LeftToRight;
+
+            FWindow.Anchor = AnchorStyles.Left | AnchorStyles.Top | AnchorStyles.Right;
+            FWindow.Dock = DockStyle.Fill;
+
+            FWindow.AutoScroll = true;
+            FWindow.BorderStyle = BorderStyle.Fixed3D;
+            //FWindow.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+
+            FWindow.AllowDrop = true;
+            FWindow.DragEnter += new DragEventHandler(InsideDragEnter);
+            FWindow.DragDrop += new DragEventHandler(InsideDragDrop);
+
+            //			FWindow.MouseDown += tableLayoutPanel1_MouseDown;
+
+            //add the main table panel to the window
+            Controls.Add(FWindow);
+        }
 
 
-        protected abstract void HandleConfigChange(IDiffSpread<string> configSpread);
-        public abstract void Evaluate(int SpreadMax);
+        #region dynamic control layout
+
+        void LayoutByCount()
+        {
+            var window = FWindow.Controls;
+
+            FWindow.Parent.SuspendLayout();
+
+            window.Clear();
+
+
+            for (var i = 0; i < 10; i++)
+            {
+                var row = new RowPanel("item " + i, false);
+                var c = row.Controls;
+
+                window.Add(row);
+            }
+
+            FWindow.Parent.ResumeLayout(false);
+            //			FMainPanel.PerformLayout();
+            //FMainPanel.Refresh();
+
+        }
+        #endregion dynamic control layout
+
+        #region drag and drop
+        void InsideDragDrop(object sender, DragEventArgs e)
+        {
+            var data = (RowPanel)e.Data.GetData(typeof(RowPanel));
+            FlowLayoutPanel _destination = (FlowLayoutPanel)sender;
+            FlowLayoutPanel _source = (FlowLayoutPanel)data.Parent;
+
+            if (_source != _destination)
+            {
+                // Add control to panel
+                _destination.Controls.Add(data);
+                data.Size = new Size(_destination.Width, 50);
+
+                // Reorder
+                Point p = _destination.PointToClient(new Point(e.X, e.Y));
+                var item = _destination.GetChildAtPoint(p);
+                int index = _destination.Controls.GetChildIndex(item, false);
+                _destination.Controls.SetChildIndex(data, index);
+
+                // Invalidate to paint!
+                _destination.Invalidate();
+                _source.Invalidate();
+            }
+            else
+            {
+                // Just add the control to the new panel.
+                // No need to remove from the other panel, this changes the Control.Parent property.
+                Point p = _destination.PointToClient(new Point(e.X, e.Y));
+                var item = _destination.GetChildAtPoint(p);
+                int index = _destination.Controls.GetChildIndex(item, false);
+                _destination.Controls.SetChildIndex(data, index);
+                _destination.Invalidate();
+            }
+        }
+
+        void InsideDragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Move;
+        }
+        #endregion drag and drop
     }
 }
---
+++ src/MessageNodes/Configurable/Abstract/RowPanel.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace VVVV.Packs.Messaging.Nodes
+{
+        class RowPanel : TableLayoutPanel
+        {
+            #region fields and properties
+            private CheckBox _toggle;
+            private TextBox _text;
+
+            public bool AllowDrag { get; set; }
+            private bool _isDragging = false;
+
+            private int _DDradius = 40;
+            private int _mX = 0;
+            private int _mY = 0;
+
+            public bool Checked
+            {
+                get
+                {
+                    return _toggle.Checked;
+                }
+                set
+                {
+                    _toggle.Checked = value;
+                }
+            }
+
+            public string Description
+            {
+                get
+                {
+                    return _text.Text;
+                }
+                set
+                {
+                    _text.Text = value;
+                }
+            }
+            #endregion fields and properties
+
+            public RowPanel()
+            {
+                this.AutoSize = true;
+                this.Anchor = AnchorStyles.Left | AnchorStyles.Right; //| AnchorStyles.Right
+
+                this.Height = 27;
+                this.MinimumSize = new Size(300, 27);
+                this.Dock = DockStyle.Fill;
+
+                this.BorderStyle = BorderStyle.FixedSingle;
+                this.BackColor = Color.FromArgb(230, 230, 230);
+
+                this.ColumnCount = 2;
+                this.RowCount = 1;
+
+                this.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20));
+                this.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100));
+
+
+                _toggle = new CheckBox();
+                LayoutToggle(_toggle);
+                this.Controls.Add(_toggle);
+
+
+                _text = new TextBox();
+                LayoutText(_text);
+                this.Controls.Add(_text);
+
+                AllowDrag = true;
+
+            }
+
+            public RowPanel(string text, bool isChecked)
+                : this()
+            {
+                Checked = isChecked;
+                Description = text;
+            }
+
+            void LayoutToggle(CheckBox box)
+            {
+                box.Text = "";
+                box.FlatStyle = FlatStyle.Standard;
+                box.AutoCheck = true;
+                box.Dock = DockStyle.Top;
+                box.Height = 18;
+                box.Width = 20;
+            }
+
+            void LayoutText(TextBox box)
+            {
+                box.ReadOnly = true;
+                box.Dock = DockStyle.Top;
+                box.Text = "Lorem ipsum";
+                box.BorderStyle = BorderStyle.None;
+                box.BackColor = Color.FromArgb(230, 230, 230);
+            }
+
+            protected override void OnGotFocus(EventArgs e)
+            {
+                _text.BackColor =
+                this.BackColor = Color.Silver;
+                base.OnGotFocus(e);
+            }
+
+            protected override void OnLostFocus(EventArgs e)
+            {
+                this.BackColor =
+                _text.BackColor = Color.FromArgb(230, 230, 230);
+                base.OnLostFocus(e);
+            }
+
+            protected override void OnClick(EventArgs e)
+            {
+                this.Focus();
+                base.OnClick(e);
+            }
+
+            protected override void OnMouseDown(MouseEventArgs e)
+            {
+                this.Focus();
+                base.OnMouseDown(e);
+                _mX = e.X;
+                _mY = e.Y;
+                this._isDragging = false;
+            }
+
+            protected override void OnMouseMove(MouseEventArgs e)
+            {
+                if (!_isDragging)
+                {
+                    // This is a check to see if the mouse is moving while pressed.
+                    // Without this, the DragDrop is fired directly when the control is clicked, now you have to drag a few pixels first.
+                    if (e.Button == MouseButtons.Left && _DDradius > 0 && this.AllowDrag)
+                    {
+                        int num1 = _mX - e.X;
+                        int num2 = _mY - e.Y;
+                        if (((num1 * num1) + (num2 * num2)) > _DDradius)
+                        {
+                            DoDragDrop(this, DragDropEffects.All);
+                            _isDragging = true;
+                            return;
+                        }
+                    }
+                    base.OnMouseMove(e);
+                }
+            }
+
+            protected override void OnMouseUp(MouseEventArgs e)
+            {
+                _isDragging = false;
+                base.OnMouseUp(e);
+            }
+
+
+        }
+    }
+
