8515454eeee4050fa15d86d4d5ec21e69ef6b144
velcrome <marko@intolight.de>
2017-04-16 20:14:06 +0200

    redo spreading on [Write]

--- src/MessageNodes/Configurable/MessageWriteNode.cs
+++ src/MessageNodes/Configurable/MessageWriteNode.cs
@@ -13,23 +13,31 @@ using VVVV.Utils;
 
 namespace VVVV.Packs.Messaging.Nodes
 {
-    [PluginInfo(Name = "Write", AutoEvaluate = true, Category = "Message", Help = "Writes into Fields of arbitrary Type", Version = "Dynamic", Tags = "Formular, Bin", Author = "velcrome")]
-    public class MessageWriteNode : TypeablePinNode, IDX11ResourceDataRetriever
+    [PluginInfo(Name = "Write", AutoEvaluate = true, Category = "Message", Help = "Writes into Fields of arbitrary Type. ", Version = "Dynamic", Tags = "Formular, Bin", Author = "velcrome")]
+    public class MessageWritNode : TypeablePinNode, IDX11ResourceDataRetriever
     {
+        [Input("Key", DefaultString = "Foo", Order = 2, BinOrder = 3, BinSize = -1, BinVisibility = PinVisibility.OnlyInspector)]
+        public new ISpread<ISpread<string>> FKey;
+
         [Input("Force", Order = int.MaxValue - 1, IsSingle = true, IsToggle = true, DefaultBoolean = true, Visibility = PinVisibility.Hidden)]
         protected IDiffSpread<bool> FForce;
 
         [Input("Update", IsToggle = true, Order = int.MaxValue, DefaultBoolean = true)]
         protected IDiffSpread<bool> FUpdate;
 
+        [Output("Iterations", Order = int.MaxValue)]
+        protected ISpread<int> FIterations;
+
         protected override IOAttribute DefinePin(FormularFieldDescriptor field)
         {
             var attr = new InputAttribute("Field");
             attr.BinVisibility = PinVisibility.Hidden;
-            
+
             attr.Order = 4;
             attr.BinOrder = 5;
             attr.BinSize = 1;
+
+            attr.BinVisibility = PinVisibility.Hidden;
 
             attr.CheckIfChanged = true;
 
@@ -61,52 +69,34 @@ namespace VVVV.Packs.Messaging.Nodes
             }
             else if (!anyUpdate) return;
 
-            var keyCount = FKey.SliceCount;
-            var ValueSpread = FValue.ToISpread();
+            var dataSpread = FValue.ToISpread();
 
-            bool newData = ValueSpread.IsChanged; // changed pin
+            bool newData = dataSpread.IsChanged; // changed pin
             newData |= FForce[0]; // assume newData, if AutoSense is off.
 
-            if (anyUpdate && newData) {
-                SpreadMax = FInput.SliceCount;
+            if (anyUpdate && newData)
+            {
+                // Update and Force not included in spreading.
+                SpreadMax = dataSpread.SliceCount.CombineWith(FKey).CombineWith(FInput);
+                if (FUpdate.IsAnyInvalid() || FForce.IsAnyInvalid()) SpreadMax = 0;
+
+                FIterations[0] = SpreadMax;
+
                 doFlush = true;
 
-                for (int i = 0; i < SpreadMax; i++)
+                var fieldIndex = 0;
+                for (int i = 0; i < SpreadMax; i++) // iterate spreaded (except update, which gets cut short)
                 {
                     Message message = FInput[i];
-                       
-                    var keyIndex = 0;
-                    foreach (var key in FKey)
+                    
+                    for (int j = 0; j < FKey[i].SliceCount; j++, fieldIndex++) // iterate all relevant fields for each message
                     {
-                        var fieldIndex = i * keyCount + keyIndex;
-                        keyIndex++;
+                        if (!FUpdate[fieldIndex]) continue; // update is per-field
 
-                        if (!FUpdate[fieldIndex]) continue;
+                        var key = FKey[i][j];
+                        var spread = dataSpread[fieldIndex] as ISpread;
 
-                        if (!message.Fields.Contains(key)) message[key] = BinFactory.New(TargetDynamicType);
-                        var spread = ValueSpread[fieldIndex] as ISpread;
-
-                        if (spread.SliceCount > 0)
-                        {
-                            if (message[key].GetInnerType().IsAssignableFrom(TargetDynamicType))
-                            {
-                                // check if any relevant change occurred
-                                if (!message[key].Equals(spread as IEnumerable)) message.AssignFrom(key, spread);
-                            }
-                            else
-                            {
-                                var targetType = message[key].GetInnerType();
-                                var newBin = BinFactory.New(targetType, spread.SliceCount);
-
-                                for(int j = 0; j< spread.SliceCount;j++)
-                                {
-                                    newBin[j] = Convert.ChangeType(spread[j], targetType);
-                                }
-
-                                if (!message[key].Equals(newBin)) message.AssignFrom(key, newBin);
-                            }
-                        }
-                        else message[key].Clear();
+                        LazyInsert(message, key, spread);
                     }
                 }
             }
@@ -117,6 +107,32 @@ namespace VVVV.Packs.Messaging.Nodes
             }
         }
 
+        private void LazyInsert(Message message, string fieldName, ISpread spread)
+        {
+            if (!message.Fields.Contains(fieldName)) message[fieldName] = BinFactory.New(TargetDynamicType);
+
+            if (spread.SliceCount > 0)
+            {
+                if (message[fieldName].GetInnerType().IsAssignableFrom(TargetDynamicType))
+                {
+                    // check if any relevant change occurred
+                    if (!message[fieldName].Equals(spread as IEnumerable)) message.AssignFrom(fieldName, spread);
+                }
+                else
+                {
+                    var targetType = message[fieldName].GetInnerType();
+                    var newBin = BinFactory.New(targetType, spread.SliceCount);
+
+                    for (int k = 0; k < spread.SliceCount; k++) // iterate all elements in a field
+                    {
+                        newBin[k] = Convert.ChangeType(spread[k], targetType);
+                    }
+
+                    if (!message[fieldName].Equals(newBin)) message.AssignFrom(fieldName, newBin);
+                }
+            }
+            else message[fieldName].Clear();
+        }
     }
 
 }
